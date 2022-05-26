c62cd0935614c563fb94275cab8bcf6ef2a741dd
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2022-05-26 01:41:46 -0400

    Hello_World!.py
    Python: Hello World!

---
+++ Hello_World!.py
@@ -0,0 +1,23 @@
+#Like a virgin touched for the very first time, welcome to the world!
+import string
+
+
+world = "Hello World!"
+
+
+def func_world():
+    print("Hello World!")
+
+
+
+print("Hello World!\n")
+
+func_world()
+
+print(world)
+
+for n in world:
+    print(world[n])
+
+if world.endswith("!"):
+    print("Hello World!\n")
