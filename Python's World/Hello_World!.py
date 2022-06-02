abba27b524ac492d1f4d70b5748a2fbbe81800d6
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2022-06-02 03:01:03 -0400

    Hello_World!.py
    Fixed issue with copy function by explicitly declaring module. Experimenting with classes and functions in the string module.

--- Python's World/Hello_World!.py
+++ Python's World/Hello_World!.py
@@ -1,12 +1,13 @@
 #Like a virgin touched for the very first time, welcome to the world!
 import string
+import copy
 
 
 world = "Hello World!"
 
 
 def func_world():
-    print("Hello World!")
+    print("Hello World!\n")
 
 
 
@@ -14,10 +15,26 @@ print("Hello World!\n")
 
 func_world()
 
-print(world)
+print(world+"\n")
 
 for n in world:
-    print(world[n])
+    print(n+"\n")
 
 if world.endswith("!"):
     print("Hello World!\n")
+
+i = 0
+for n in world:
+    print(world[i])
+    i += 1
+
+#shallow copy
+new_world = copy.copy(world)
+print(new_world+"\n")
+
+#deep copy
+deep_new_world = copy.deepcopy(world)
+print(deep_new_world+"\n")
+
+ascii_world = string.ascii_letters(world)
+print(ascii_world+"\n")
